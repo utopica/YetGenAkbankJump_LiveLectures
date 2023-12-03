5f01153eae691e8e7fdd00f326a777d85dcc4432
utopica <h.elifokumus@gmail.com>
2023-12-03 18:35:20 +0300

    Configured entity mappings using Fluent API in 'OnModelCreating', applying configurations from the current assembly.

---
+++ Week10_1/Week10_1.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Week10_1.Domain.Entities;
+using Week10_1.Persistence.Configurations;
+
+namespace Week10_1.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.ApplyConfiguration(new StudentConfiguration());
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //modelBuilder.Ignore<User>();
+            //modelBuilder.Ignore<Role>();
+            //modelBuilder.Ignore<UserSetting>();
+
+
+        }
+    }
+}
