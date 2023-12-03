b008a530c0fbcbc81abe9f1796bcf1eda06ef9ae
utopica <h.elifokumus@gmail.com>
2023-12-03 19:09:40 +0300

    Deleted "Product" Entities

--- Week10_1/Week10_1.Domain/Entities/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Week10_1.Domain.Entities
-{
-    public class Product : EntityBase<Guid>
-    {
-        public string Name { get; set; }
-
-        public ICollection<ProductCategory> ProductCategories { get; set; }
-    }
-}
