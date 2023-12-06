0d5131eff2ba7f16ec6cb3616d4cfe885dfed685
utopica <h.elifokumus@gmail.com>
2023-12-06 13:51:16 +0300

    Added Associated Configuration Files For Identities

---
+++ Week10_1/Week10_1.Persistence/Configurations/Identity/UserSettingConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Week10_1.Domain.Entities;
+using Week10_1.Domain.Identity;
+
+namespace Week10_1.Persistence.Configurations.Identity
+{
+    public class UserSettingConfiguration : IEntityTypeConfiguration<UserSetting>
+    {
+        public UserSetting Id { get; set; }
+
+        public void Configure(EntityTypeBuilder<UserSetting> builder)
+        {
+            // Relationships
+
+            builder.HasOne<User>(x => x.User)
+                .WithOne(u => u.UserSetting)
+                .HasForeignKey<UserSetting>(u => u.UserId);
+
+
+
+
+            // CreatedByUserId
+            builder.Property(x => x.CreatedByUserId).IsRequired();
+            builder.Property(x => x.CreatedByUserId).HasMaxLength(75);
+
+            // CreatedOn
+            builder.Property(x => x.CreatedOn).IsRequired();
+
+            // ModifiedByUserId
+            builder.Property(x => x.ModifiedByUserId).IsRequired(false);
+            builder.Property(x => x.ModifiedByUserId).HasMaxLength(75);
+
+            // LastModifiedOn
+            builder.Property(x => x.LastModifiedOn).IsRequired(false);
+
+            builder.ToTable("UserSettings");
+        }
+    }
+}
---
+++ Week10_1/Week10_1.Persistence/Contexts/IdentityContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Week10_1.Domain.Identity;
+
+namespace Week10_1.Persistence.Contexts
+{
+    public class IdentityContext :IdentityDbContext<User, Role,Guid>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //modelBuilder.Ignore<User>();
+            //modelBuilder.Ignore<Role>();
+            //modelBuilder.Ignore<UserSetting>();
+
+
+        }
+    }
+}
