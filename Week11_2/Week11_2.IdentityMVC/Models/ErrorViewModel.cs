7d3f76213fc77582fa99e34eff51b054f509e358
utopica <h.elifokumus@gmail.com>
2023-12-07 01:00:42 +0300

    Created Identity.MVC project

---
+++ Week11_2/Week11_2.IdentityMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Week11_2.IdentityMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
