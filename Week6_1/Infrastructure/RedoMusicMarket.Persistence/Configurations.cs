759918e7c3f9e229d048b2e1e6b84ff20bdb11ed
utopica <h.elifokumus@gmail.com>
2023-12-07 17:58:11 +0300

    Added Infrastructure Layer

---
+++ Week6_1/Infrastructure/RedoMusicMarket.Persistence/Configurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoMusicMarket.Persistence
+{
+    public static class Configurations
+    {
+        public static string GetStringFromJson(string key)
+        {
+            ConfigurationManager configurationManager = new();
+            string path = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName}\\Infrastructure\\ReDoMusicMarket.Persistence";
+
+            configurationManager.SetBasePath(path);
+
+            //reading
+            configurationManager.AddJsonFile("PrivateInformations.json");
+
+            return configurationManager.GetSection(key).Value;
+        }
+    }
+}
---
+++ Week6_1/Infrastructure/RedoMusicMarket.Persistence/Contexts/ReDoMusicMarketDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedoMusicMarket.Domain.Entities;
+using RedoMusicMarket.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusicMarket.Persistence.Contexts
+{
+    public class ReDoMusicMarketDbContext : DbContext
+    {
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configurations.GetStringFromJson("ConnectionStrings:PostgreSQL"));
+        }
+
+    }
+}
