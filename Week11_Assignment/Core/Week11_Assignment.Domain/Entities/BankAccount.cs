94ccf155d7b8df865ef96bcfa7e797ff793066e4
utopica <h.elifokumus@gmail.com>
2023-12-07 10:45:58 +0300

    Added Common Entities and BankAccount Entity

---
+++ Week11_Assignment/Core/Week11_Assignment.Domain/Entities/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Week11_Assignment.Domain.Common;
+
+namespace Week11_Assignment.Domain.Entities
+{
+    public class BankAccount : EntityBase<Guid>
+    {
+ 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+
+    }
+}
